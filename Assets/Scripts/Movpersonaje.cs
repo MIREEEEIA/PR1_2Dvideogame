12693f5a9b88412f38072570ec08b6f4bc7fd930
mirefontanars@gmail.com <mirefontanars@gmail.com>
2024-04-15 10:38:01 +0200

    Del video 5 al 11

--- Assets/Scripts/Movpersonaje.cs
+++ Assets/Scripts/Movpersonaje.cs
@@ -16,21 +16,40 @@ public class Movpersonaje : MonoBehaviour
     void Start()
     {
        rb = GetComponent<Rigidbody2D>();
+
+       transform.position = new Vector3(-9.1f, 3.0f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movTeclas = Input.GetAxis("Horizontal"); //(a -1f - d 1f)
-        //float movTeclasY = Input.GetAxis("Vertical"); //(a -1f - d 1f)
-
-
-        float miDeltaTime = Time.deltaTime;
+         float miDeltaTime = Time.deltaTime;
 
         //movimiento personaje
+        float movTeclas = Input.GetAxis("Horizontal"); //(a -1f - d 1f) 
+        //float movTeclasY = Input.GetAxis("Vertical"); //(a -1f - d 1f)
+
         rb.velocity = new Vector2(movTeclas*multiplicador, rb.velocity.y);
 
+        //flip <--
+        if(movTeclas < 0){
+          this.GetComponent<SpriteRenderer>().flipX = true;  
+        }else if(movTeclas > 0){
+          this.GetComponent<SpriteRenderer>().flipX = false;  
+        }
+        
+
         //salto
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.5f);
+        Debug.DrawRay(transform.position, Vector2.down, Color.magenta);
+
+        if(hit){
+            puedoSaltar = true;
+            Debug.Log(hit.collider.name);
+        }else{
+            puedoSaltar = false;
+        }
+
         if(Input.GetKeyDown(KeyCode.Space) && puedoSaltar ){
           rb.AddForce(
             new Vector2(0,multiplicadorSalto),
@@ -40,10 +59,10 @@ public class Movpersonaje : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(){
+    /*void OnCollisionEnter2D(){
         puedoSaltar = true;
         Debug.Log("Collision");
-    }
+    }*/
     
 
     //Esto es un comentario para actualizar github
